17c8fa53a8cd4756188b23b6246e8d622c56beda
Benjamin Rainer <benjamin.rainer@itec.aau.at>
2024-02-28 07:09:58 +0100

    vectorized regularized dirac

--- regularizedDirac.m
+++ regularizedDirac.m
@@ -1,7 +1,8 @@
 function [v] = regularizedDirac(gamma,x)
-v = 0;
-if abs(x)<=2*gamma
-    v = 1/(4*gamma) .* (cos((pi.*x)./(2.*gamma) + 1));
-end
+v = zeros(1,size(x,2));
+idx = abs(x) <= 2*gamma;
+
+v(idx) = 1/(4*gamma) .* (cos((pi.*x(idx))./(2.*gamma)) + 1);
+
 end
 
