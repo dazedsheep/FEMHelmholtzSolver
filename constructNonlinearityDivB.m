897219bd4e92185851e789578f45108d8fc6a7b1
Benjamin Rainer <benjamin.rainer@itec.aau.at>
2026-02-17 20:23:04 +0100

    Update constructNonlinearityDivB.m

--- constructNonlinearityDivB.m
+++ constructNonlinearityDivB.m
@@ -1,7 +1,7 @@
 function [f] = constructNonlinearityDivB(elements, massDensity, speed_of_sound, diffusivityDomain, diffusivityPhantoms, centers, radii, values, sourceValueDomain, convection)
 
 if convection == true
-    f  = (1+sourceValueDomain).*ones(size(elements.points,1),1)./(2.*massDensity.*speed_of_sound.*diffusivityDomain);
+    f  = (1+sourceValueDomain).*ones(size(elements.points,1),1)./(2.*massDensity.*speed_of_sound^2.*diffusivityDomain);
 else
     f = zeros(size(elements.points,1),1);
 end
@@ -11,13 +11,13 @@ for j=1:size(centers,2)
         % this is a point source
         % find nearest node to impose our point source
         [v,pcenterIdx] = min(sum((elements.points - centers(:,j)').^2,2)); 
-        f(pcenterIdx) =  (1+1./2.*values(j))./(2.*massDensity.*speed_of_sound.*diffusivityPhantoms(j));
+        f(pcenterIdx) =  (1+1./2.*values(j))./(2.*massDensity.*speed_of_sound^2.*diffusivityPhantoms(j));
     else
         % this is a "disc" source
         if abs(values(j)) > 0
             for i=1:size(elements.points,1)
                 if norm(elements.points(i,:) - centers(:,j)',2) < radii(j) 
-                    f(i) = (1+ 1./2.*values(j))./(2.*massDensity.*speed_of_sound.*diffusivityPhantoms(j));
+                    f(i) = (1+ 1./2.*values(j))./(2.*massDensity.*speed_of_sound^2.*diffusivityPhantoms(j));
                 end
             end
         end
